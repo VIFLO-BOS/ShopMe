9098e86fb229ed1fde4f6e42b405b3df7908d335
banks <adeniyisunday2244@gmail.com>
2024-12-21 20:15:50 +0100

    Making a change to the hamburger menu

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+
+
+
+// THIS IS TO GET THE TOGGLE BUTTON TO WORK 
+
+const bars = document.getElementById('open-ico');
+const fallBars = document.getElementById('close-ico');
+const dropdown = document.getElementById('navbar-content');
+
+bars.addEventListener('click', () => {
+    // alert('i got clicked boss')
+    bars.classList.toggle('hidden');
+    fallBars.classList.toggle('hidden');
+    dropdown.classList.add('visible');
+});
+
+fallBars.addEventListener('click', () => { // Corrected the syntax here
+    bars.classList.toggle('hidden'); // Changed 'visible' to 'hidden' for consistency
+    fallBars.classList.toggle('hidden');
+    dropdown.classList.remove('visible');
+});
+
+
