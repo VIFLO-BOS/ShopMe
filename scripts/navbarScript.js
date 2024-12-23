2c55fbc459d0e9ce8d2908e006f79082eb82d22d
Bankole Olaniyi Sunday <adeniyisunday2244@gmail.com>
2024-12-23 05:55:39 +0100

    adding modifiled scripts

--- scripts/navbarScript.js
+++ scripts/navbarScript.js
@@ -17,3 +17,23 @@ fallBars.addEventListener('click', () => {
     dropdown.classList.remove('visible');
     dropdown.classList.add('hidden');
 });
+
+// THIS IS ALSO TO TOGGLE THE DROP-MENU ON THE MENU LINK
+const accTogler= document.getElementById('acc-btn');
+const shopTogler = document.getElementById('shop-btn');
+
+const shopmenu = document.getElementById('shop-menu');
+const accmenu = document.getElementById('acc-menu');
+
+accTogler.addEventListener('click', () => {
+    accmenu.classList.toggle('visible');
+
+});
+
+shopTogler.addEventListener('click', () => {
+    shopmenu.classList.toggle('visible');
+});
+
+
+
+
